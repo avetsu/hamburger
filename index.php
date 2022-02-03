aaf9670c94dba5dc71a3c5ba99bfc46c52992616
avetsu <70340986+avetsu@users.noreply.github.com>
2022-02-03 18:25:40 +0100

    index.php
    index idk

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+
+<html>
+
+  <head>
+  
+    <meta charset="utf-8">
+  
+  </head>
+  
+  <body>
+  
+  
+    <form action="rdr.php" method="post">
+
+    	<input type="text" name="user" value="Username" required></br></br>	
+		
+		<input type="password" name="pwd" value="Password" required></br></br></br>
+		
+		<input type="submit" value="Submit">
+
+    </form>
+    
+  
+  </body>
+
+</html>
